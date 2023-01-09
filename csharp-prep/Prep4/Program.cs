210f1b4dc27ac05be6e48c61ec9f0d82d95747ca
WillyMayer1 <willianmayercl@gmail.com>
2023-01-09 15:21:04 -0300

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        //Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        int myNum = -1;
+        while (myNum != 0) {
+            
+            Console.Write("Enter a list of numbers, type 0 when finished. ");
+            
+            string response = Console.ReadLine();
+            myNum = int.Parse(response);
+
+            if (myNum != 0) {
+                numbers.Add(myNum);
+            }
+        }
+
+        Console.WriteLine("");
+        int sum = 0;
+        foreach (int number in numbers) {
+            sum = sum + number;
+        }
+
+        Console.WriteLine($"The sum is {sum}.");
+    
+        int max = 0;
+        foreach (int number in numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is {max}.");
+    }   
 }
